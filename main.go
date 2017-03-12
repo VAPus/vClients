143f485780d4929c7e627ec0dc55ba0c5e1e9c0c
Raggaer <nakotoffana@gmail.com>
2017-03-12 22:54:52 +0100

    HTTP wrapper

--- main.go
+++ main.go
@@ -7,14 +7,17 @@ import (
 	"fmt"
 )
 
+var appHandler = applicationHandler{}
+
 type applicationHandler struct {
 	Config *config
+	Response http.ResponseWriter
+	Request *http.Request
 }
 
-func main() {
-	// Create main application handler
-	appHandler := applicationHandler{}
+type controller func(handler applicationHandler) (int, error)
 
+func main() {
 	cfg, err := loadConfigurationFile("config.toml")
 
 	if err != nil {
@@ -25,6 +28,9 @@ func main() {
 
 	// Create router
 	router := httprouter.New()
+	router.GET("/", wrapper(clientList))
+
+	log.Printf("Listening on %v:%v", appHandler.Config.Host, appHandler.Config.Port)
 
 	// Listen
 	if appHandler.Config.SSL.Enabled {
@@ -44,6 +50,29 @@ func main() {
 	}
 }
 
-func wrapper() {
+func clientList(app applicationHandler) (int, error) {
+	app.Response.Write([]byte("1212"))
 
+	return 200, nil
+}
+
+func wrapper(c controller) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		// Create controller handler
+		handler := applicationHandler{
+			Config: appHandler.Config,
+			Request: r,
+			Response: w,
+		}
+
+		// Call controller
+		status, err := c(handler)
+
+		if err != nil {
+			http.Error(w, err.Error(), status)
+			return
+		}
+
+		w.WriteHeader(status)
+	}
 }
